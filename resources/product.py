457f553691185cdbdb9330a9f4d4d4decaa618c8
Tommi Leiritie <topselius@gmail.com>
2020-11-26 16:47:56 +0200

    add product resource

---
+++ resources/product.py
@@ -0,0 +1,69 @@
+import os
+
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from http import HTTPStatus
+
+from models.product import Product
+
+
+class ProductResource(Resource):
+
+    @jwt_required
+    def get(self, product_id):
+
+        product = Product.get_by_id(product_id=product_id)
+
+        if product is None:
+            return {'message': 'Product not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        return json_data, HTTPStatus.OK
+
+    @jwt_required
+    def patch(self, product_id):
+
+        json_data = request.get_json()
+
+        product = Product.get_by_id(product_id=product_id)
+
+        if product is None:
+            return {'message': 'Product not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != product.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        product.name = data.get('name') or product.name
+        product.description = data.get('description') or product.description
+        product.stock = data.get('num_of_servings') or product.stock
+        product.price = data.get('cook_time') or product.price
+        product.size = data.get('ingredients') or product.size
+
+        product.save()
+
+        clear_cache('/products')
+
+        return json_data, HTTPStatus.OK
+
+    @jwt_required
+    def delete(self, product_id):
+
+        product = Product.get_by_id(product_id=product_id)
+
+        if product is None:
+            return {'message': 'Product not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user != product.user_id:
+            return {'message': 'Access is not allowed'}, HTTPStatus.FORBIDDEN
+
+        product.delete()
+
+        clear_cache('/products')
+
+        return {}, HTTPStatus.NO_CONTENT
