d5147e573baa721225171177c3373cc0d81f4551
Santeri Hautala <71129078+Sunttu152@users.noreply.github.com>
2020-12-10 20:50:37 +0200

    Created login.js file and updated login.html

---
+++ templates/login.js
@@ -0,0 +1,12 @@
+function getToken(){
+
+    
+    var x = document.getElementById("username").value;
+    var y = document.getElementById("password").value;
+    
+    var x = {"username":"Matti"}
+
+    var y = {"password":"testi"}
+
+
+};
