4d10c0ba540b53923f80ee622c7576431633a9dd
Tommi Leiritie <topselius@gmail.com>
2020-12-13 19:25:34 +0200

    enable timestamps in user schema

--- schemas/user.py
+++ schemas/user.py
@@ -15,5 +15,5 @@ class UserSchema(Schema):
     password = fields.String(required=True, validate=[validate.Length(min=4,max=50)])
     is_active = fields.Boolean(dump_only=True)
 
-    #created_at = fields.DateTime(dump_only=True)
-    #updated_at = fields.DateTime(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
